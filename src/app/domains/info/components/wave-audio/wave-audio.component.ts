81eeac910f73d72dd72154be8975eeee9c5dda1f
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2024-09-10 15:09:40 -0400

    Uso de directivas

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,9 +1,10 @@
+import { HighlightDirective } from '@/shared/directives/highlight.directive';
 import { Component, ElementRef, Input, signal, ViewChild } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
 @Component({
 	selector: 'app-wave-audio',
 	standalone: true,
-	imports: [],
+	imports: [HighlightDirective],
 	templateUrl: './wave-audio.component.html',
 	styleUrl: './wave-audio.component.css',
 })
